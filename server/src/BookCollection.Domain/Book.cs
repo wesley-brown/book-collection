840cc08bf11ebdc869396e8e91881732fc4cb3d4
Wesley Brown <wesleybrown@wesleybrown.net>
2020-10-08 17:08:27 -0400

    Ensure Identical Books Have Same Hash Codes
    Identical books should have the same hash codes since they will be considered
    value types by this REST API.

---
+++ server/src/BookCollection.Domain/Book.cs
@@ -0,0 +1,37 @@
+﻿namespace BookCollection.Domain
+{
+    /// <summary>
+    /// A book.
+    /// </summary>
+    public sealed class Book
+    {
+        /// <summary>
+        /// The title of this book.
+        /// </summary>
+        public string Title { get; }
+
+        /// <summary>
+        /// The author of this book.
+        /// </summary>
+        public Author Author { get; }
+
+        /// <summary>
+        /// Create a book with a given title and author.
+        /// </summary>
+        /// <param name="title">The title of the book.</param>
+        /// <param name="author">The author of the book.</param>
+        public Book(string title, Author author)
+        {
+            Title = title;
+            Author = author;
+        }
+
+        public override int GetHashCode()
+        {
+            int hash = 7;
+            hash = 31 * hash + Title.GetHashCode();
+            hash = 31 * hash + Author.GetHashCode();
+            return hash;
+        }
+    }
+}
---
+++ server/tests/BookCollection.Domain.Tests/When_comparing_books.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace BookCollection.Domain.Tests
+{
+    public sealed class When_comparing_books
+    {
+        [Fact]
+        public void identical_books_have_the_same_hash_codes()
+        {
+            var jRRTolkein = new Author("J. R. R. Tolkein");
+            var bookOne = new Book("The Hobbit", jRRTolkein);
+            var bookTwo = new Book("The Hobbit", jRRTolkein);
+            Assert.Equal(bookOne.GetHashCode(), bookTwo.GetHashCode());
+        }
+    }
+}
